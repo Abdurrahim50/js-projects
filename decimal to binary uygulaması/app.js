3766c56197a530ce6bfa61f214939d7f848c117a
Abdurrahim50 <errahim0001@gmail.com>
2025-03-26 00:05:22 +0300

    Yeni klasörler eklendi ve proje yapısı güncellendi

--- decimal to binary uygulaması/app.js
+++ decimal to binary uygulaması/app.js
@@ -1,14 +1,10 @@
-let sayi = Number(prompt("Bir sayı giriniz"));
+let binary ="1011";
 
-let binary = decimal(sayi);
-console.log("İkilik sistemde sayı (ters çevrilmiş) : " + binary);
-
-function decimal(number) {
-    let binary = "";
-    while (number > 0) {
-        binary += (number % 2).toString();  // Sayıyı 2'ye bölüp kalanı ekliyoruz
-        number = Math.floor(number / 2);   // Sayıyı 2'ye bölüyoruz (tam sayı bölme)
+function convert(binary){
+    let toplam=0;
+    for(let i=binary.length-1;i>=0;i--){
+        toplam+=Math.pow(i,2);
     }
-    let reversed = binary.split('').reverse().join('');  // Elde ettiğimiz binary'i tersine çeviriyoruz
-    return reversed;  // Sonuç döndürülüyor
+    return toplam;
 }
+convert();
